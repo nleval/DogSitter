bd517acdd1c4d17116b98fb341b2fb2a1e9d61f3
Julian Campistron <julian.campistron@gmail.com>
2025-11-05 15:50:05 +0100

    Ajout d'affichage d'un avis et plusieurs avis

--- include.php
+++ include.php
@@ -14,8 +14,11 @@ require_once 'controllers/controller_factory.class.php';
 require_once 'controllers/controller.class.php';
 require_once 'controllers/controller_index.class.php';
 require_once 'controllers/controller_utilisateur.class.php';
+require_once 'controllers/controller_avis.class.php';
 
 //Ajout des modèles
 require_once 'modeles/Bd.class.php';
 require_once 'modeles/Utilisateur.class.php';
 require_once 'modeles/Utilisateur.dao.php';
+require_once 'modeles/Avis.class.php';
+require_once 'modeles/Avis.dao.php';
