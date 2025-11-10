39eba00fbae705486127a2c3039f5dbeae471286
Julian Campistron <julian.campistron@gmail.com>
2025-11-10 11:08:15 +0100

    Ajout type variables dans Avis.class.php

--- modeles/Avis.class.php
+++ modeles/Avis.class.php
@@ -2,15 +2,21 @@
 
 class Avis
 {
-    private $id_avis;
-    private $note;
-    private $texte_commentaire;
-    private $id_utilisateur;
-    private $id_promenade;
-    private $id_utilisateur_note;
+    private ?int $id_avis;
+    private ?string $note;
+    private ?string $texte_commentaire;
+    private ?int $id_utilisateur;
+    private ?int $id_promenade;
+    private ?int $id_utilisateur_note;
 
-    public function __construct($id_avis, $note, $texte_commentaire, $id_utilisateur, $id_promenade, $id_utilisateur_note)
-    {
+    public function __construct(
+        ?int $id_avis = null, 
+        $note = null, 
+        $texte_commentaire = null, 
+        ?int $id_utilisateur = null, 
+        ?int $id_promenade = null, 
+        ?int $id_utilisateur_note = null
+    ){
         $this->id_avis = $id_avis;
         $this->note = $note;
         $this->texte_commentaire = $texte_commentaire;
@@ -21,50 +27,50 @@ class Avis
 
     //GETTEUR & SETTEUR
     // ID
-    public function getId() {
+    public function getId(): ?int {
         return $this->id_avis;
     }
-    public function setId($id_avis): void {
+    public function setId(?int $id_avis): void {
         $this->id_avis = $id_avis;
     }
 
     // Note
-    public function getNote() {
+    public function getNote(): ?string {
         return $this->note;
     }
-    public function setNote($note): void {
+    public function setNote(?string $note): void {
         $this->note = $note;
     }
 
     // Texte commentaire
-    public function getTexteCommentaire() {
+    public function getTexteCommentaire(): ?string {
         return $this->texte_commentaire;
     }
-    public function setTexteCommentaire($texte_commentaire): void {
+    public function setTexteCommentaire(?string $texte_commentaire): void {
         $this->texte_commentaire = $texte_commentaire;
     }
 
     // Id utilisateur
-    public function getIdUtilisateur() {
+    public function getIdUtilisateur(): ?int {
         return $this->id_utilisateur;
     }
-    public function setIdUtilisateur($id_utilisateur): void {
+    public function setIdUtilisateur(?int $id_utilisateur): void {
         $this->id_utilisateur = $id_utilisateur;
     }
 
     // Id promenade
-    public function getIdPromenade() {
+    public function getIdPromenade(): ?int {
         return $this->id_promenade;
     }
-    public function setIdPromenade($id_promenade): void {
+    public function setIdPromenade(?int $id_promenade): void {
         $this->id_promenade = $id_promenade;
     }
 
     // Id utilisateur note
-    public function getIdUtilisateurNote() {
+    public function getIdUtilisateurNote(): ?int {
         return $this->id_utilisateur_note;
     }
-    public function setIdUtilisateurNote($id_utilisateur_note): void {
+    public function setIdUtilisateurNote(?int $id_utilisateur_note): void {
         $this->id_utilisateur_note = $id_utilisateur_note;
     }
 }
