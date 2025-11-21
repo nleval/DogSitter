258161c1adc23f40fe59d1bcf76ef2935afc9bfa
Lilian Tieys <ltieys@univ-pau.fr>
2025-11-21 14:51:50 +0100

    lilian chien

--- modeles/chien.dao.php
+++ modeles/chien.dao.php
@@ -31,10 +31,6 @@ class ChienDAO{
                 $row['id_utilisateur']
             );
             
-<<<<<<< HEAD
-=======
-   
->>>>>>> bd7f2461d36bfc0914d1eb0182c277318fc48dfb
             
         }
         return null;
@@ -55,10 +51,6 @@ class ChienDAO{
                 $row['cheminPhoto'],
                 $row['id_utilisateur']
             );
-<<<<<<< HEAD
-=======
-            
->>>>>>> bd7f2461d36bfc0914d1eb0182c277318fc48dfb
         }
         return $chiens;
     }
