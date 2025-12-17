168b47995a3514c8ae949dc95a74aa9abef1aba9
Lilian Tieys <ltieys@univ-pau.fr>
2025-12-17 11:28:57 +0100

    pas fini

--- controllers/controller_chien.class.php
+++ controllers/controller_chien.class.php
@@ -13,16 +13,16 @@ class ControllerChien extends Controller
         $id_chien = isset($_GET['id_chien']) ? (int)$_GET['id_chien'] : null;
         $chien = $managerchien->findById($id_chien); // Exemple avec l'ID 1
 
-        $managerutilisateur = new AvisDAO($this->getPDO());
-        $propriétaire = $managerutilisateur->findById(1); // Exemple avec l'ID 1
-        var_dump($propriétaire);
+        // $managerutilisateur = new UtilisateurDAO($this->getPDO());
+        // $propriétaire = $managerutilisateur->findById(1); // Exemple avec l'ID 1
+        // var_dump($propriétaire);
         
 
         // Rendre la vue avec le chien
         $template = $this->getTwig()->load('chien.html.twig');
         echo $template->render([
             'chien' => $chien,
-            'proprietaire' => $propriétaire
+            // 'proprietaire' => $propriétaire
         ]);
     }
 
