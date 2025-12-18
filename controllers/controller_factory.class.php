b971a2c3209f9d502b08fbedd1b1ce559a22efd4
Noah Leval <noah.leval@gmail.com>
2025-12-18 12:10:36 +0100

    controller factory

--- controllers/controller_factory.class.php
+++ controllers/controller_factory.class.php
@@ -1,7 +1,20 @@
 <?php
-
+/**
+ * @file controller_factory.class.php
+ * @author Léval Noah
+ * @brief Factory pour les contrôleurs
+ * @details Cette classe permet de créer un contrôleur en fonction de son nom
+ * @version 1.0
+ * @date 2025-12-18
+ */
 class ControllerFactory
 {
+    /**
+     * @brief Permet de récupérer une instance de contrôleur en fonction de son nom
+     * @param string $controleur Nom du contrôleur à instancier.
+     * @param \Twig\Environment $twig Moteur de templates Twig.
+     * @param \Twig\Loader\FilesystemLoader $loader Chargeur de templates Twig.
+     */
     public static function getController($controleur, \Twig\Loader\FilesystemLoader $loader, \Twig\Environment $twig)
     {
         $controllerName="Controller".ucfirst($controleur);
