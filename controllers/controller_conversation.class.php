70a8eb994e39f5bb800494941379665a074c9fe7
Aymeric Pigeon <apigeon001@iutbayonne.univ-pau.fr>
2026-01-12 08:37:49 +0100

    Front des messages et conversation refaits

--- controllers/controller_conversation.class.php
+++ controllers/controller_conversation.class.php
@@ -8,11 +8,11 @@ class ControllerConversation extends Controller
 
     public function afficherConversation()
     {
-        // Récupérer un utilisateur spécifique depuis la base de données
+        // Récupérer une conversation spécifique depuis la base de données
         $managerconversation = new ConversationDAO($this->getPDO());
         $conversation = $managerconversation->findById(1); // Exemple avec l'ID 1
 
-        // Rendre la vue avec l'utilisateur
+        // Rendre la vue avec la conversation
         $template = $this->getTwig()->load('conversation.html.twig');
         echo $template->render([
             'conversation' => $conversation
